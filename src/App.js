7ea20dda55cf624cc6729ec9fa42623b1ba96477
shivansh tyagi <stshivansh9@gmail.com>
2021-01-14 11:37:09 +0530

    changed to offline fonts

--- src/App.js
+++ src/App.js
@@ -1,105 +1,117 @@
-import React, {Component} from 'react'
-import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom'
-import RoutePath from "./lib/RoutePath";
-import MomentUtils from "@date-io/moment";
-import {toast, ToastContainer} from 'material-react-toastify'
-import "material-react-toastify/dist/ReactToastify.css";
-import {connect} from "react-redux";
-import UserActions from "./redux/actions/UserActions";
-import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
-import withStyles from "@material-ui/core/styles/withStyles";
-import {MuiPickersUtilsProvider} from '@material-ui/pickers';
-import {CssBaseline} from "@material-ui/core";
-import {ThemeProvider} from "@material-ui/styles";
-import LightStylesConfig from "./lib/StyleConfigs/ColorsConfig/LightStylesConfig";
-import DarkStylesConfig from "./lib/StyleConfigs/ColorsConfig/DarkStylesConfig";
-import LandingPage from "./components/LandingPage/LandingPage";
-import Navbar from "./components/Navbar/Navbar";
-import AppStyles from "./AppStyles";
+import React, { Component } from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import RoutePath from './lib/RoutePath';
+import MomentUtils from '@date-io/moment';
+import { toast, ToastContainer } from 'material-react-toastify';
+import 'material-react-toastify/dist/ReactToastify.css';
+import { connect } from 'react-redux';
+import UserActions from './redux/actions/UserActions';
+import withStyles from '@material-ui/core/styles/withStyles';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import LightStylesConfig from './lib/StyleConfigs/ColorsConfig/LightStylesConfig';
+import DarkStylesConfig from './lib/StyleConfigs/ColorsConfig/DarkStylesConfig';
+import LandingPage from './components/LandingPage/LandingPage';
+import Navbar from './components/Navbar/Navbar';
+import AppStyles from './AppStyles';
+import './assets/fonts/material-fonts/material-fonts.css';
+import './assets/fonts/montserrat/montserrat.css';
 
-const lightTheme = createMuiTheme(LightStylesConfig);
+const lightTheme = LightStylesConfig;
 
-const darkTheme = createMuiTheme(DarkStylesConfig);
+const darkTheme = DarkStylesConfig;
 
-const styles = AppStyles
+const styles = AppStyles;
 
 const toastConfiguration = {
-   autoClose: 20000,
-   // draggable: true,
-   // pauseOnHover: true
-   //etc you get the idea
-}
+  autoClose: 2000,
+  // draggable: true,
+  // pauseOnHover: true
+  //etc you get the idea
+};
 
 let PrivateRoute = (props) => {
-   let {component: Component, authorized, ...rest} = props;
-   return (
-      <Route
-         {...rest}
-         component={
-            (props) => authorized
-               ? <Component {...props} {...rest} />
-               : <Redirect to={{pathname: RoutePath.loginPath, state: {from: props.location}}}/>
-         }
-      />
-   )
+  let { component: Component, authorized, ...rest } = props;
+  return (
+    <Route
+      {...rest}
+      component={(props) =>
+        authorized ? (
+          <Component {...props} {...rest} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: RoutePath.loginPath,
+              state: { from: props.location },
+            }}
+          />
+        )
+      }
+    />
+  );
 };
 
 class App extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         loggedIn: true,
-         prefersDarkTheme: false
-      }
-   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: true,
+      prefersDarkTheme: false,
+    };
+  }
 
-   render() {
-      const {classes,isDarkMode} = this.props;
-      if (this.state.loggedIn === undefined) {
-         return (
-            <>
-            </>
-         )
-      } else {
-         return (
-            <>
-               <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-                  <div className={classes.root}>
-                     <CssBaseline/>
-                     <MuiPickersUtilsProvider utils={MomentUtils}>
-                        <BrowserRouter>
-                           <Navbar/>
-                           <Switch>
-                              {/*<Route exact path={RoutePath.loginPath} component={LoginContainer}/>*/}
-                              <PrivateRoute exact path={RoutePath.homePath} authorized={this.props.loggedIn}
-                                            component={LandingPage}/>
-                              {/*<PrivateRoute exact path={RoutePath.detailedList()} authorized={this.props.loggedIn}*/}
-                              {/*              component={DetailedList}/>*/}
-                           </Switch>
-                        </BrowserRouter>
-                     </MuiPickersUtilsProvider>
-                  </div>
-               </ThemeProvider>
-               <ToastContainer {...toastConfiguration} style={{marginTop: 40}}/>
-            </>
-         )
-      }
-   }
+  render() {
+    const { classes, isDarkMode } = this.props;
+    if (this.state.loggedIn === undefined) {
+      return <></>;
+    } else {
+      return (
+        <>
+          <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+            <div className={classes.root}>
+              <CssBaseline />
+              <MuiPickersUtilsProvider utils={MomentUtils}>
+                <BrowserRouter>
+                  <Navbar />
+                  <Switch>
+                    {/*<Route exact path={RoutePath.loginPath} component={LoginContainer}/>*/}
+                    <PrivateRoute
+                      exact
+                      path={RoutePath.homePath}
+                      authorized={this.props.loggedIn}
+                      component={LandingPage}
+                    />
+                    {/*<PrivateRoute exact path={RoutePath.detailedList()} authorized={this.props.loggedIn}*/}
+                    {/*              component={DetailedList}/>*/}
+                  </Switch>
+                </BrowserRouter>
+              </MuiPickersUtilsProvider>
+            </div>
+          </ThemeProvider>
+          <ToastContainer {...toastConfiguration} style={{ marginTop: 40 }} />
+        </>
+      );
+    }
+  }
 }
 
 let mapStateToProps = (state) => {
-   return {
-      loggedIn: true,
-      // loggedIn: state.GeneralReducer.loggedIn,
-      // loggedUser: state.GeneralReducer.loggedUser
-      isDarkMode: state.SettingsReducer.isDarkMode
-   }
+  return {
+    loggedIn: true,
+    // loggedIn: state.GeneralReducer.loggedIn,
+    // loggedUser: state.GeneralReducer.loggedUser
+    isDarkMode: state.SettingsReducer.isDarkMode,
+  };
 };
 
 let mapDispatchToProps = (dispatch) => {
-   return {
-      loginUser: data => dispatch(UserActions.loginUser(data))
-   }
-}
+  return {
+    loginUser: (data) => dispatch(UserActions.loginUser(data)),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(App))
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(App));
---
+++ src/components/LoginContainer/LoginContainer.js
@@ -0,0 +1,162 @@
+import React, { Component } from 'react';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { toast } from 'react-toastify';
+import RoutePath from '../../lib/RoutePath';
+import { connect } from 'react-redux';
+import userService from '../../services/userService';
+import withStyles from '@material-ui/core/styles/withStyles';
+import UserActions from '../../redux/actions/UserActions';
+import LoginContainerStyles from './LoginContainerStyles';
+
+let styles = LoginContainerStyles;
+
+class LoginContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      // email: 'demo@awiros.com',
+      // password: 'awisys555',
+    };
+  }
+
+  componentDidMount = () => {
+    this.checkLoggedIn();
+  };
+
+  componentDidUpdate = (prevProps, prevState, snapshot) => {
+    this.checkLoggedIn();
+  };
+
+  checkLoggedIn = () => {
+    if (this.props.loggedIn) {
+      RoutePath.navigateTo(this.props, RoutePath.homePath);
+    }
+  };
+
+  handleLogin = async () => {
+    // e.preventDefault();
+    let { email, password } = this.state;
+    const response = await userService.login({ email, password });
+    // console.log(response);
+    if (response.success) {
+      toast.success('Login successful');
+      this.props.loginUser({ token: response.data });
+      RoutePath.navigateTo(this.props, RoutePath.homePath);
+    } else {
+      toast.error(response.message);
+    }
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.handleLogin();
+  };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    let { classes } = this.props;
+    let { email, password } = this.state;
+    return (
+      <div className={classes.root}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <form
+              className={classes.form}
+              onSubmit={this.handleSubmit}
+              noValidate
+            >
+              <TextField
+                required
+                autoFocus
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                label="Email Address"
+                id="email"
+                name="email"
+                value={email}
+                autoComplete="email"
+                onChange={this.handleChange}
+              />
+              <TextField
+                required
+                fullWidth
+                variant="outlined"
+                margin="normal"
+                label="Password"
+                name="password"
+                type="password"
+                id="password"
+                value={password}
+                autoComplete="current-password"
+                onChange={this.handleChange}
+              />
+              {/*<FormControlLabel*/}
+              {/*   control={<Checkbox value="remember" color="primary"/>}*/}
+              {/*   label="Remember me"*/}
+              {/*/>*/}
+              <div style={{ height: this.props.theme.spacing(5) }}></div>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                // onClick={this.handleLogin}
+                // className={classes.submit}
+              >
+                Sign In
+              </Button>
+              {/*<Grid container>*/}
+              {/*   <Grid item xs>*/}
+              {/*      <Link href="#" variant="body2">*/}
+              {/*         Forgot password?*/}
+              {/*      </Link>*/}
+              {/*   </Grid>*/}
+              {/*   <Grid item>*/}
+              {/*      <Link href="#" variant="body2">*/}
+              {/*         {"Don't have an account? Sign Up"}*/}
+              {/*      </Link>*/}
+              {/*   </Grid>*/}
+              {/*</Grid>*/}
+            </form>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+}
+
+let mapStateToProps = (state) => {
+  return {
+    loggedIn: state.UserReducer.loggedIn,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loginUser: (data) => dispatch(UserActions.loginUser(data)),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles, { withTheme: true })(LoginContainer));
