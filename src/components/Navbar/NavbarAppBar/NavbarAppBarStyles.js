fbe436e3d00038fd77c9eab15941185d4dda1c71
shivansh tyagi <stshivansh9@gmail.com>
2021-03-08 17:28:19 +0530

    added styles

--- src/components/Navbar/NavbarAppBar/NavbarAppBarStyles.js
+++ src/components/Navbar/NavbarAppBar/NavbarAppBarStyles.js
@@ -1,5 +1,78 @@
 let NavbarAppBarStyles = (theme) => ({
-    root: {}
-})
+  root: {},
+  grow: {
+    flexGrow: 1,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+
+  menuButton: {
+    marginRight: 36,
+  },
+  logoText: {
+    display: 'block',
+  },
+  logoTextHideOnLogoDisplay: {
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  logoImageContainer: {
+    // display: 'block',
+    width: '100%',
+    //  [theme.breakpoints.down('xs')]: {
+    //    display: 'none',
+    //  },
+  },
+  logoImage: {
+    width: '100%',
+    maxWidth: 225,
+    [theme.breakpoints.down('xs')]: {
+      // height: 20,
+      maxWidth: 175,
+    },
+    // width:'100%'
+  },
+  poweredByAwirosImage: {
+    width: '100%',
+    maxWidth: 135,
+    // position: 'absolute',
+    // right: 120,
+    // top: 13,
+    // bottom: 0,
+
+    [theme.breakpoints.down('xs')]: {
+      maxWidth: 115,
+      // right: 10,
+    },
+    //  marginLeft:'50vw'
+    // width:'100%'
+  },
+  siteSelectorContainer: {
+    width: 170,
+  },
+
+  dropdownMenuContainer: {
+    zIndex: theme.zIndex.drawer + 2,
+  },
+  dropdownMenu: {
+    marginTop: 5,
+    marginRight: 20,
+    borderTop: `3px solid`,
+    borderColor: theme.palette.primary.main,
+    '& ul': {
+      margin: 0,
+      padding: 0,
+    },
+  },
+});
 
 export default NavbarAppBarStyles;
--- src/components/Navbar/NavbarStyles.js
+++ src/components/Navbar/NavbarStyles.js
@@ -8,60 +8,8 @@ let NavbarStyles = (theme) => ({
   root: {
     display: 'flex',
   },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
   grow: {
     flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  logoText: {
-    display: 'block',
-  },
-  logoTextHideOnLogoDisplay: {
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
-    },
-  },
-  logoImageContainer: {
-    // display: 'block',
-    width: '100%',
-    //  [theme.breakpoints.down('xs')]: {
-    //    display: 'none',
-    //  },
-  },
-  logoImage: {
-    width: '100%',
-    maxWidth: 225,
-    [theme.breakpoints.down('xs')]: {
-      // height: 20,
-      maxWidth: 175,
-    },
-    // width:'100%'
-  },
-  poweredByAwirosImage: {
-    width: '100%',
-    maxWidth: 135,
-    // position: 'absolute',
-    // right: 120,
-    // top: 13,
-    // bottom: 0,
-
-    [theme.breakpoints.down('xs')]: {
-      maxWidth: 115,
-      // right: 10,
-    },
-    //  marginLeft:'50vw'
-    // width:'100%'
-  },
-  siteSelectorContainer: {
-    width: 170,
   },
 
   mobileAppBar: {
@@ -197,20 +145,6 @@ let NavbarStyles = (theme) => ({
 
   buttonsContainer: {
     marginRight: 20,
-  },
-
-  dropdownMenuContainer: {
-    zIndex: theme.zIndex.drawer + 2,
-  },
-  dropdownMenu: {
-    marginTop: 5,
-    marginRight: 20,
-    borderTop: `3px solid`,
-    borderColor: theme.palette.primary.main,
-    '& ul': {
-      margin: 0,
-      padding: 0,
-    },
   },
 
   content: {
